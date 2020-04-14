5c9d1467330d35d0e1497073c53e1670f27e9906
MykhailKo <tuareg1812@gmail.com>
2020-04-14 13:01:56 +0300

    Prefinal commit 1

--- vivatech_django/apps/home/views.py
+++ vivatech_django/apps/home/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.conf import settings
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core import mail
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
+from django.utils.translation import LANGUAGE_SESSION_KEY
 from django.urls import reverse
 from .models import Slide
 from .models import Slider
@@ -10,6 +12,22 @@ from news.models import News
 
 
 def home_slides_news(request):
+    lang_codes = [c for (c, name) in settings.LANGUAGES]
+    parts = request.get_full_path().split('/')
+   # try:
+   #     if request.session[LANGUAGE_SESSION_KEY]: 
+   #         request.session[LANGUAGE_SESSION_KEY] = request.session[LANGUAGE_SESSION_KEY] 
+   # except:
+   #     request.session[LANGUAGE_SESSION_KEY] = 'uk'
+    if parts[1] == 'en':
+        parts[1] = 'uk'
+        return redirect('/'.join(parts))
+   # elif parts[1] == 'ru' and request.session[LANGUAGE_SESSION_KEY] != 'uk':
+   #     parts[1] = 'ru'
+   #     return redirect('/'.join(parts))
+    elif parts[1] not in lang_codes:
+        parts[0] = '/' + 'uk'
+        return redirect('/'.join(parts))
     slides = Slider.objects.all().order_by('slide_order_number')
     news = News.objects.all().order_by('-pub_date')[0:3]
     return render(request, 'home/slides_news.html', {'slides': slides, 'news': news})
